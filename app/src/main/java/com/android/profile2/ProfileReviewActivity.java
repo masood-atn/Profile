8279e720ad1331d1dd914bd0a58e31a2fa8d5be3
Masood <masood.atn@hotmail.com>
2019-10-24 03:12:26 +0330

    create & early design all activities

---
+++ app/src/main/java/com/android/profile2/ProfileReviewActivity.java
@@ -0,0 +1,14 @@
+package com.android.profile2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ProfileReviewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile_review);
+    }
+}
