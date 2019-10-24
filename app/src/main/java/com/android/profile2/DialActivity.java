52dc1ba8cf725c187cec296f2d2cac226993e7f5
Masood <masood.atn@hotmail.com>
2019-10-24 03:50:28 +0330

    define logic for dial activity

--- app/src/main/java/com/android/profile2/DialActivity.java
+++ app/src/main/java/com/android/profile2/DialActivity.java
@@ -2,9 +2,12 @@ package com.android.profile2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class DialActivity extends AppCompatActivity {
 
@@ -14,15 +17,18 @@ public class DialActivity extends AppCompatActivity {
         setContentView(R.layout.activity_dial);
 
 
-    Button btnDial = findViewById(R.id.btnDial);
+        Button btnDial = findViewById(R.id.btnDial);
+        final EditText edtDial = findViewById(R.id.edtDial);
 
 
-    btnDial.setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View view) {
-
-        }
-    });
+        btnDial.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Uri phoneNumber = Uri.parse("tel:" + edtDial.getText().toString());
+                Intent i = new Intent(Intent.ACTION_DIAL, phoneNumber);
+                startActivity(i);
+            }
+        });
 
     }
 }
