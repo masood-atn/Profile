0ed6d1ef26f4f26645069353e6f2c94e7c10a77f
Masood <masood.atn@hotmail.com>
2019-10-24 15:23:23 +0330

    define logic for profile review & profile edit

--- app/src/main/java/com/android/profile2/ProfileActivity.java
+++ app/src/main/java/com/android/profile2/ProfileActivity.java
@@ -1,11 +1,14 @@
 package com.android.profile2;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class ProfileActivity extends AppCompatActivity {
 
@@ -13,16 +16,56 @@ public class ProfileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        final EditText edtFirstName = findViewById(R.id.edtFirstName);
+        final EditText edtLastName = findViewById(R.id.edtLastName);
+        final EditText edtEmail = findViewById(R.id.edtEmail);
+        final EditText edtAge = findViewById(R.id.edtAge);
 
         Button btnReview = findViewById(R.id.btnReview);
-
 
         btnReview.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+                String firstName = edtFirstName.getText().toString();
+                String lastName = edtLastName.getText().toString();
+                String email = edtEmail.getText().toString();
+                String age = edtAge.getText().toString();
+
                 Intent i = new Intent(ProfileActivity.this, ProfileReviewActivity.class);
-                startActivity(i);
+                i.putExtra("firstName", firstName);
+                i.putExtra("lastName", lastName);
+                i.putExtra("email", email);
+                i.putExtra("age", age);
+                startActivityForResult(i, 200);
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        final EditText edtFirstName = findViewById(R.id.edtFirstName);
+        final EditText edtLastName = findViewById(R.id.edtLastName);
+        final EditText edtEmail = findViewById(R.id.edtEmail);
+        final EditText edtAge = findViewById(R.id.edtAge);
+
+        if (requestCode == 200) {
+            if (resultCode == Activity.RESULT_OK) {
+
+                String userFirstName = data.getStringExtra("firstName");
+                String userLastName = data.getStringExtra("lastName");
+                String userEmail = data.getStringExtra("email");
+                String userAge = data.getStringExtra("age");
+
+                edtFirstName.setText(userFirstName);
+                edtLastName.setText(userLastName);
+                edtEmail.setText(userEmail);
+                edtAge.setText(userAge);
+
+            }
+
+        }
     }
 }
--- app/src/main/java/com/android/profile2/ProfileReviewActivity.java
+++ app/src/main/java/com/android/profile2/ProfileReviewActivity.java
@@ -2,10 +2,12 @@ package com.android.profile2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class ProfileReviewActivity extends AppCompatActivity {
 
@@ -13,10 +15,25 @@ public class ProfileReviewActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_review);
+        Intent i = getIntent();
+
+        final String firstName = i.getStringExtra("firstName");
+        final String lastName = i.getStringExtra("lastName");
+        final String email = i.getStringExtra("email");
+        final String age = i.getStringExtra("age");
+
+        TextView txtFirstName = findViewById(R.id.txtFirstName);
+        TextView txtLastName = findViewById(R.id.txtLastName);
+        TextView txtEmail = findViewById(R.id.txtEmail);
+        TextView txtAge = findViewById(R.id.txtAge);
+
+        txtFirstName.setText(firstName);
+        txtLastName.setText(lastName);
+        txtEmail.setText(email);
+        txtAge.setText(age);
 
         Button btnConfirm = findViewById(R.id.btnConfirm);
         Button btnEdit = findViewById(R.id.btnEdit);
-
 
         btnConfirm.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -28,8 +45,13 @@ public class ProfileReviewActivity extends AppCompatActivity {
         btnEdit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent i = new Intent(ProfileReviewActivity.this, ProfileActivity.class);
-                startActivity(i);
+                Intent returnIntent = new Intent();
+                returnIntent.putExtra("firstName", firstName);
+                returnIntent.putExtra("lastName", lastName);
+                returnIntent.putExtra("email", email);
+                returnIntent.putExtra("age", age);
+                setResult(Activity.RESULT_OK, returnIntent);
+                finish();
             }
         });
     }
