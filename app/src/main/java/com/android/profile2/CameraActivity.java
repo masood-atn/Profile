024eb7d709f2968f612ebeece7a812e6ee582670
Masood <masood.atn@hotmail.com>
2019-11-15 03:26:14 +0330

    some changes

--- app/src/main/java/com/android/profile2/CameraActivity.java
+++ app/src/main/java/com/android/profile2/CameraActivity.java
@@ -1,38 +1,86 @@
 package com.android.profile2;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.Environment;
+import android.view.View;
+import android.widget.Button;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
-import top.defaults.camera.CameraView;
-import top.defaults.camera.Photographer;
-import top.defaults.camera.PhotographerFactory;
+import com.camerakit.CameraKit;
+import com.camerakit.CameraKitView;
 
+import java.io.File;
+import java.io.FileOutputStream;
 
 public class CameraActivity extends AppCompatActivity {
-
-    CameraView preview = findViewById(R.id.preview);
-    Photographer photographer = PhotographerFactory.createPhotographerWithCamera2(this, preview);
-
+    private CameraKitView cameraKitView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        photographer.startPreview();
+        setContentView(R.layout.activity_camera);
+      //  cameraKitView = findViewById(R.id.camera);
+        Button btnCamera = findViewById(R.id.btnCamera);
+        btnCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (ContextCompat.checkSelfPermission(CameraActivity.this,
+                        Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(CameraActivity.this, new String[]{Manifest.permission.CAMERA}, 1500);
+                } else {
+                    cameraKitView.captureImage(new CameraKitView.ImageCallback() {
+                        @Override
+                        public void onImage(CameraKitView cameraKitView, final byte[] capturedImage) {
+                            File savedPhoto = new File(Environment.getRootDirectory(), "photo.jpg");
+                            try {
+                                FileOutputStream outputStream = new FileOutputStream(savedPhoto.getPath());
+                                outputStream.write(capturedImage);
+                                outputStream.close();
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    });
+                }
 
+            }
+        });
+    }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        cameraKitView.onStart();
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        photographer.startPreview();
+        cameraKitView.onResume();
     }
 
     @Override
     protected void onPause() {
-        photographer.stopPreview();
         super.onPause();
+        cameraKitView.onPause();
     }
 
+    @Override
+    protected void onStop() {
+        super.onStop();
+        cameraKitView.onStop();
+    }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        cameraKitView.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
 }
